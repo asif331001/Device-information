1d1aed1a270114669156371e0acf0aadfc2270b8
asif331001 <asif95331001@gmail.com>
2024-05-19 11:02:39 +0600

    android id

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:device_info_plus/device_info_plus.dart';
-import 'dart:io' show Platform;
-import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'dart:io';
+import 'package:android_id/android_id.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: DeviceInfoScreen(),
     );
   }
@@ -31,86 +32,100 @@ class _DeviceInfoScreenState extends State<DeviceInfoScreen> {
   String deviceName = '';
   String deviceModel = '';
   String deviceManufacturer = '';
+  static const _androidIdPlugin = AndroidId();
+  String _androidId = 'Unknown';
 
   @override
   void initState() {
     super.initState();
     _initDeviceInfo();
+    _initAndroidId();
   }
 
   Future<void> _initDeviceInfo() async {
     final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
-    String deviceType;
-    String deviceUniqueId;
-    String deviceName;
-    String deviceModel;
-    String deviceManufacturer;
 
     try {
       if (Platform.isAndroid) {
         final AndroidDeviceInfo androidInfo = await deviceInfoPlugin.androidInfo;
-        deviceType = 'Android';
-        deviceUniqueId = androidInfo.id ?? 'Unknown';
-        deviceName = androidInfo.model ?? 'Unknown';
-        deviceModel = androidInfo.device ?? 'Unknown';
-        deviceManufacturer = androidInfo.manufacturer ?? 'Unknown';
+        setState(() {
+          deviceType = 'Android';
+          deviceUniqueId = androidInfo.id ?? 'Unknown';
+          deviceName = androidInfo.brand ?? 'Unknown';
+          deviceModel = androidInfo.model ?? 'Unknown';
+          deviceManufacturer = androidInfo.manufacturer ?? 'Unknown';
+        });
       } else if (Platform.isIOS) {
         final IosDeviceInfo iosInfo = await deviceInfoPlugin.iosInfo;
-        deviceType = 'iOS';
-        deviceUniqueId = iosInfo.identifierForVendor ?? 'Unknown';
-        deviceName = iosInfo.name ?? 'Unknown';
-        deviceModel = iosInfo.model ?? 'Unknown';
-        deviceManufacturer = 'Apple';
+        setState(() {
+          deviceType = 'iOS';
+          deviceUniqueId = iosInfo.identifierForVendor ?? 'Unknown';
+          deviceName = iosInfo.name ?? 'Unknown';
+          deviceModel = iosInfo.model ?? 'Unknown';
+          deviceManufacturer = 'Apple';
+        });
       } else if (Platform.isMacOS) {
         final MacOsDeviceInfo macInfo = await deviceInfoPlugin.macOsInfo;
-        deviceType = 'macOS';
-        deviceUniqueId = macInfo.systemGUID ?? 'Unknown';
-        deviceName = macInfo.model ?? 'Unknown';
-        deviceModel = macInfo.model ?? 'Unknown';
-        deviceManufacturer = 'Apple';
+        setState(() {
+          deviceType = 'macOS';
+          deviceUniqueId = macInfo.systemGUID ?? 'Unknown';
+          deviceName = macInfo.model ?? 'Unknown';
+          deviceModel = macInfo.model ?? 'Unknown';
+          deviceManufacturer = 'Apple';
+        });
       } else if (Platform.isWindows) {
         final WindowsDeviceInfo windowsInfo = await deviceInfoPlugin.windowsInfo;
-        deviceType = 'Windows';
-        deviceUniqueId = windowsInfo.deviceId ?? 'Unknown';
-        deviceName = windowsInfo.computerName ?? 'Unknown';
-        deviceModel = 'Unknown'; // Windows devices don't have a specific model
-        deviceManufacturer = 'Unknown'; // Windows devices don't have a specific manufacturer
+        setState(() {
+          deviceType = 'Windows';
+          deviceUniqueId = windowsInfo.deviceId ?? 'Unknown';
+          deviceName = windowsInfo.computerName ?? 'Unknown';
+          deviceModel = 'Unknown';
+          deviceManufacturer = 'Unknown';
+        });
       } else if (Platform.isLinux) {
         final LinuxDeviceInfo linuxInfo = await deviceInfoPlugin.linuxInfo;
-        deviceType = 'Linux';
-        deviceUniqueId = linuxInfo.machineId ?? 'Unknown';
-        deviceName = linuxInfo.name ?? 'Unknown';
-        deviceModel = linuxInfo.name ?? 'Unknown';
-        deviceManufacturer = 'Unknown'; // Linux devices typically don't have a specific manufacturer
-      } else if (kIsWeb) {
-        final WebBrowserInfo webInfo = await deviceInfoPlugin.webBrowserInfo;
-        deviceType = 'Web';
-        deviceUniqueId = '${webInfo.vendor ?? 'Unknown'} ${webInfo.userAgent ?? 'Unknown'}';
-        deviceName = webInfo.browserName.name ?? 'Unknown';
-        deviceModel = 'Unknown'; // Web devices don't have a specific model
-        deviceManufacturer = webInfo.vendor ?? 'Unknown';
+        setState(() {
+          deviceType = 'Linux';
+          deviceUniqueId = linuxInfo.machineId ?? 'Unknown';
+          deviceName = linuxInfo.name ?? 'Unknown';
+          deviceModel = linuxInfo.name ?? 'Unknown';
+          deviceManufacturer = 'Unknown';
+        });
       } else {
-        deviceType = 'Unknown';
-        deviceUniqueId = 'Unknown';
-        deviceName = 'Unknown';
-        deviceModel = 'Unknown';
-        deviceManufacturer = 'Unknown';
+        setState(() {
+          deviceType = 'Unknown';
+          deviceUniqueId = 'Unknown';
+          deviceName = 'Unknown';
+          deviceModel = 'Unknown';
+          deviceManufacturer = 'Unknown';
+        });
       }
     } catch (e) {
-      deviceType = 'Error';
-      deviceUniqueId = 'Error';
-      deviceName = 'Error';
-      deviceModel = 'Error';
-      deviceManufacturer = 'Error';
+      setState(() {
+        deviceType = 'Error';
+        deviceUniqueId = 'Error';
+        deviceName = 'Error';
+        deviceModel = 'Error';
+        deviceManufacturer = 'Error';
+      });
     }
+  }
 
-    setState(() {
-      this.deviceType = deviceType;
-      this.deviceUniqueId = deviceUniqueId;
-      this.deviceName = deviceName;
-      this.deviceModel = deviceModel;
-      this.deviceManufacturer = deviceManufacturer;
-    });
+  Future<void> _initAndroidId() async {
+    try {
+      final androidId = await _androidIdPlugin.getId() ?? 'Unknown ID';
+      if (mounted) {
+        setState(() {
+          _androidId = androidId;
+        });
+      }
+    } on PlatformException {
+      if (mounted) {
+        setState(() {
+          _androidId = 'Failed to get Android ID.';
+        });
+      }
+    }
   }
 
   @override
@@ -129,9 +144,11 @@ class _DeviceInfoScreenState extends State<DeviceInfoScreen> {
             Text('Device Unique ID: $deviceUniqueId'),
             const SizedBox(height: 8),
             Text('Device Name: $deviceName'),
-            SizedBox(height: 8),
+            const SizedBox(height: 8),
             Text('Device Model: $deviceModel'),
-            SizedBox(height: 8),
+            const SizedBox(height: 8),
+            if (Platform.isAndroid) Text('Android Device ID: $_androidId'),
+            const SizedBox(height: 8),
             Text('Device Manufacturer: $deviceManufacturer'),
           ],
         ),
@@ -139,6 +156,3 @@ class _DeviceInfoScreenState extends State<DeviceInfoScreen> {
     );
   }
 }
-
-
-
